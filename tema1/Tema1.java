04e3c3c3ea071ee1b9c57d7c96eca75865930e6c
Julio Hamburger <jhamburger79@gmail.com>
2022-09-10 13:13:33 -0500

    Ejercicios curso java básico OpenBootcamp

---
+++ tema1/Tema1.java
@@ -0,0 +1,29 @@
+public class Tema1 {
+  public static void main(String[] args) {
+    //enteros
+    byte num1 = 2;
+    short num2 = 12;
+    int entero1 = 100;
+    long entero2 = 34633378;
+    System.out.println("\n+- Tipo de datos enteros:");
+    System.out.print("byte: " + num1 + "\nshort: " + num2 + "\nint: " + entero1 + "\nlong: " + entero2 + "\n");
+
+    //decimales
+    float decimal1 = 1.34f;
+    double decimal2 = 23.456;
+    System.out.println("\n+- Tipo de datos decimal:");
+    System.out.print("float: " + decimal1 + "\ndouble: " + decimal2 + "\n");
+
+    //Booleanos
+    boolean booleano1 = false;
+    boolean booleano2 = true;
+    System.out.println("\n+- Tipo de datos booleano:");
+    System.out.print("boolean: " + booleano1 + "\nboolean: " + booleano2 + "\n");
+
+    //textos
+    char caracter = 'A';
+    String nombre = "Julio";
+    System.out.println("\n+- Tipo de dato texto:");
+    System.out.print("char: " + caracter + "\nString: " + nombre + "\n");
+  }
+}
